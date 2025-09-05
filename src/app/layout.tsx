25d051ca30845856ebba1c33f5c1a15cdfbf2dab
Elvin-lee <herjun802@gmail.com>
2025-09-05 23:55:33 +0900

    [Modified & Debug]: InfoModal import 및 Hydration error 해결 InfoModal 컴포넌트를 import함으로써 modal을 나타내고 suppressHydrationWarning 문구 추가로 Hydration error 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,16 @@ export const metadata: Metadata = {
   description: '소음 측정 데이터를 지도에 시각화하고 저장할 수 있는 서비스',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode; }) {
+export default function RootLayout({ 
+  children 
+}: Readonly<{ children: React.ReactNode; }>) {
+  
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body className="bg-white antialiased">
           <div id="modal"></div>
           {children}
+          <InfoModal />
       </body>
     </html>
   );
