e379c541dff7b3a077caf148ef51e102bb9215cb
Elvin-lee <herjun802@gmail.com>
2025-09-06 22:57:55 +0900

    [Styles]: UI확인을 위한 MeasurementCard 컴포넌트에 각 컴포넌트 import

--- src/components/measurement/measurementCard/MeasurementCard.tsx
+++ src/components/measurement/measurementCard/MeasurementCard.tsx
@@ -1,7 +1,14 @@
+import NoiseChart from "../chart/NoiseChart";
+import MeasureDecibel from "../measureDecibel/MeasureDecibel";
+import { MeasureDS } from "../measureDescription/MeasureDS";
 import MeasureInfo from "../measureInfo/MeasureInfo";
+import CancelButton from "../measurementButton/cancelButton/CancelButton";
+import PostMeasurementBtn from "../measurementButton/postMeasurementActions/PostMeasurementBtn";
+import StartButton from "../measurementButton/startButton/StartButton";
 
 export default function MeasurementCard() {
     return (
+        <>
         <div 
             className="
                 w-full
@@ -9,6 +16,7 @@ export default function MeasurementCard() {
                 pt-[0.875rem]
                 px-[1rem] 
                 pb-[1.0625rem]
+                mb-[1.125rem]
                 rounded-[1rem]
                 bg-[#F4F4F4]
                 border-2 
@@ -16,6 +24,11 @@ export default function MeasurementCard() {
             "
         >
             <MeasureInfo />
+            <MeasureDecibel />
+            <NoiseChart />
+            <MeasureDS />
         </div>
+        <PostMeasurementBtn />
+        </>
     );
 }
