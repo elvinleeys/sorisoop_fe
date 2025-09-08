ba9324f1912b58d48e67bb166b91280bfb0ea9bb
Elvin-lee <herjun802@gmail.com>
2025-09-09 01:53:42 +0900

    [Feat]: Register page init

---
+++ src/app/(no-nav)/register/page.tsx
@@ -0,0 +1,12 @@
+import { RegisterHeader } from "@/components/header";
+
+export default function Register() {
+    return (
+        <>
+            <RegisterHeader />
+            <main className="px-[1rem]">
+                register page
+            </main>
+        </>
+    );
+}
