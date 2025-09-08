528b60678f235f966df3f4545b1550ff42a54b36
Elvin-lee <herjun802@gmail.com>
2025-09-08 13:31:37 +0900

    [Feat]: Chart에 decibel 및 timer 연결

--- src/components/measurement/chart/NoiseChart.tsx
+++ src/components/measurement/chart/NoiseChart.tsx
@@ -1,10 +1,17 @@
+import { useMeasurementTimer } from "@/hook/useTimer";
 import { flexRowCenter } from "@/mixin/style";
+import { useMeasurementStore } from "@/store/measurement/measurementStore";
 import { NoiseMeterChart } from "soridam-design-system";
 
 export default function NoiseChart() {
+    const { currentDecibel } = useMeasurementStore();
+    const time = useMeasurementTimer();
+
+    const formattedDecibel = Math.round(currentDecibel);
+
     return (
         <div className={`${flexRowCenter} w-full h-[13.75rem] mb-[1.0625rem]`}>
-            <NoiseMeterChart db={0} time={"00:15"}/>
+            <NoiseMeterChart db={formattedDecibel} time={time}/>
         </div>
     );
 }
