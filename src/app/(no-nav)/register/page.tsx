f956ccadea2d8222ae52e2d660db807794b90705
Elvin-lee <herjun802@gmail.com>
2025-09-10 00:38:06 +0900

    [Modified]: Register Form 및 RegisterMap import

--- src/app/(no-nav)/register/page.tsx
+++ src/app/(no-nav)/register/page.tsx
@@ -1,11 +1,14 @@
 import { RegisterHeader } from "@/components/header";
+import RegisterMap from "@/components/register/mapSection/RegisterMap";
+import RegisterForm from "@/components/register/registerForm/RegisterForm";
 
 export default function Register() {
     return (
         <>
             <RegisterHeader />
             <main className="px-[1rem]">
-                register page
+                <RegisterMap />
+                <RegisterForm />
             </main>
         </>
     );
