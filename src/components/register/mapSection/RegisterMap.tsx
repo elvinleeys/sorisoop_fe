bb69b1528ee95711675d1278b262c2c494805f36
Elvin-lee <herjun802@gmail.com>
2025-09-11 15:38:39 +0900

    [Modified]: lat,lng 정보가 없으면 소음 측정 서비스 화면으로 redirect

--- src/components/register/mapSection/RegisterMap.tsx
+++ src/components/register/mapSection/RegisterMap.tsx
@@ -3,6 +3,8 @@
 import KakaoMap from "@/components/kakaoMap/KakaoMap";
 import { useLocationStore } from "@/store/measurement/locationStore";
 import { useMeasurementStore } from "@/store/measurement/measurementStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const getDecibelImg = (db: number) => {
     if (db <= 70) return "icons/quiet.svg";
@@ -12,11 +14,21 @@ const getDecibelImg = (db: number) => {
 };
 
 export default function RegisterMap() {
+    const router = useRouter();
     const { avgDecibel } = useMeasurementStore();
     const { location } = useLocationStore();
-    const { latitude: lat, longitude: lng } = location;
+    const { latitude: lat, longitude: lng } = location || {};
 
-    if (lat === null || lng === null) return <p>위치 정보를 불러오는 중...</p>;
+    // ✅ 위치 정보 없으면 redirect
+    useEffect(() => {
+        if (!lat || !lng) {
+            router.replace("/"); // 원래 측정 페이지
+        }
+    }, [lat, lng, router]);
+
+    if (!lat || !lng) {
+        return null; // redirect 중이므로 UI 렌더링 방지
+    }
 
     const imgSrc = getDecibelImg(avgDecibel);
     const marker = [{ lat, lng, image: imgSrc }];
