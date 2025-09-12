1e76a2454de5c73600870ea00e2d1c6617d27441
Elvin-lee <herjun802@gmail.com>
2025-09-12 23:57:01 +0900

    [Feat]: MapHeader와 SearchHeader의 export 추가

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -1,2 +1,4 @@
 export { default as MainHeader } from './MainHeader';
 export { default as RegisterHeader } from './RegisterHeader';
+export { default as MapHeader } from './MapHeader';
+export { default as SearchHeader } from './SearchHeader'; 
