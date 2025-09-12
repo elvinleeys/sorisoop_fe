0f4a835df4fd6f561b2d0c831cf20b170b2dd19c
Elvin-lee <herjun802@gmail.com>
2025-09-13 00:01:00 +0900

    [Feat]: Map 서비스 화면의 MapHeader 및 FilterBottomSheet import

---
+++ src/app/(main)/map/page.tsx
@@ -0,0 +1,12 @@
+import FilterBottomSheet from "@/components/filterBottomSheet/FilterBottomSheet";
+import { MapHeader } from "@/components/header";
+
+
+export default function Map() {
+    return (
+        <>
+            <MapHeader />
+            <FilterBottomSheet />
+        </>
+    );
+}
