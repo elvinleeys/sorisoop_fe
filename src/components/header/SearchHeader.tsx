4bcadecaa400bb24ca4418e02b0e11a258108342
Elvin-lee <herjun802@gmail.com>
2025-09-12 23:55:16 +0900

    [Feat]: 장소 검색 화면의 header UI 완성 Map 서비스 화면의 장소 검색 버튼 클릭시 보이는 장소 검색 화면의 header UI 완성

---
+++ src/components/header/SearchHeader.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { flexRow, flexRowCenter } from "@/mixin/style";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useEffect, useRef } from "react";
+import { BackButton } from "soridam-design-system";
+
+type SearchBarProps = {
+  keyword: string; // 외부 상태 연결
+  setKeyword: (value: string) => void; // 입력 변경 시 상태 업데이트
+};
+
+export default function SearchHeader({keyword, setKeyword}: SearchBarProps) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, []);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setKeyword(e.target.value);
+    };
+
+    return (
+        <header 
+            className={`
+                ${flexRow}
+                w-full 
+                h-[3.9375rem]
+                pt-[0.375rem]
+                pb-[0.5625rem]
+                border-b-2
+                border-[#DDE2E5]
+            `}
+        >
+            <div className={`${flexRowCenter} w-[3rem] h-[3rem]`}>
+                <BackButton onClick={() => {router.back()}} size="md"/>
+            </div>
+            <div className="flex-1 px-[0.5rem] py-[0.75rem]">
+                <input 
+                    type="search" 
+                    ref={inputRef}
+                    value={keyword}
+                    onChange={handleChange}
+                    className="
+                        w-full 
+                        h-full 
+                        text-base 
+                        text-neutral-black 
+                        outline-none 
+                        placeholder-[#808080]
+                    "
+                    placeholder = "지번, 도로명 주소 검색"    
+                />
+            </div>
+        </header>
+    );
+}
