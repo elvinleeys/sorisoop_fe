4ebb50771ef17d7aeba5dafd5793dd40e02ac391
Elvin-lee <herjun802@gmail.com>
2025-09-13 15:48:36 +0900

    [Feat]: FilterBottomSheet의 DecibelSection(소음 수준) UI 완성

---
+++ src/components/filterBottomSheet/decibelSection/DecibelSection.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { flexCol, flexRow, flexRowCenter } from "@/mixin/style";
+import { useFilterStore } from "@/store/filter/FilterStore";
+import { DecibelButton } from "soridam-design-system";
+import { options } from "./Decibel";
+
+export default function DecibelSection() {
+  const { selectedNoiseLevels, toggleNoiseLevel } = useFilterStore();
+
+  return (
+    <div className={`${flexCol} gap-5`}>
+      <div className={`${flexRow} gap-2`}>
+        <p className="font-medium text-base text-neutral-black">
+          소음 수준
+        </p>
+        <p className={`text-[#757575] text-sm ${flexRowCenter}`}>
+          중복선택 가능
+        </p>
+      </div>
+
+      {/* 옵션 목록 */}
+      <ul className={`${flexCol} gap-4`}>
+        {options.map((option, index) => (
+          <li key={option.label} className={`${flexRow} items-center`}>
+            <DecibelButton
+              label={option.label}
+              level={option.decibelLv}
+              active={selectedNoiseLevels.includes(option.decibelLv)}
+              onClick={() => toggleNoiseLevel(option.decibelLv)}
+            />
+            {index === 0 && (
+              <span
+                className={`ml-2 text-[#757575] text-sm ${flexRowCenter}`}
+              >
+                기본값
+              </span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
