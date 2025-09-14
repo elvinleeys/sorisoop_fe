8d2aeb6b5a82d83248a22ea3781e863daefc5aca
Elvin-lee <herjun802@gmail.com>
2025-09-14 23:45:04 +0900

    [Feat]: saveDetail 화면의 header 완성

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -2,3 +2,5 @@ export { default as MainHeader } from './MainHeader';
 export { default as RegisterHeader } from './RegisterHeader';
 export { default as MapHeader } from './MapHeader';
 export { default as SearchHeader } from './SearchHeader'; 
+export { default as SaveHeader } from './SaveHeader';
+export { default as SaveDetailHeader } from './SaveDetailHeader';
