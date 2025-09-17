4768ca06c2f95c0eb666b326cfa5732cef0a1fd7
Elvin-lee <herjun802@gmail.com>
2025-09-17 22:49:20 +0900

    [Modified]: 회원가입 3 page에 import

--- src/app/(auth)/sign-up/step3/page.tsx
+++ src/app/(auth)/sign-up/step3/page.tsx
@@ -1,7 +1,9 @@
+import StepThreeForm from "@/components/sign-up/step3/StepThreeForm";
 
 export default function SignUpStepThree() {
     return (
         <>
+            <StepThreeForm />
         </>
     );
 }
