1095483230269092e64dde262ea899421fcb330d
Elvin-lee <herjun802@gmail.com>
2025-09-19 22:47:25 +0900

    [Feat]: MongoDB 연결

---
+++ src/lib/db.ts
@@ -0,0 +1,33 @@
+import mongoose, { Mongoose } from "mongoose";
+
+interface Cached {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+const MONGODB_URI: string = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(".env.local에 MONGODB_URI를 설정해주세요.");
+}
+
+/**
+ * Mongoose global 캐싱 (Next.js Hot Reload 방지)
+ */
+const cached: Cached = global.mongoose ?? { conn: null, promise: null };
+global.mongoose = cached
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
