aa6425bf8768a8d987b394c26f633546888c218d
Elvin-lee <herjun802@gmail.com>
2025-09-22 01:12:12 +0900

    [Modified]: 로그인 유무 검증 로직 추가 /register화면으로의 이동은 로그인유무에 따라 달라지므로 authStore를 통해 accessToken의 존재유무에 따라 페이지 이동하도록 변경

--- src/components/measurement/measurementButton/MeasurementButton.tsx
+++ src/components/measurement/measurementButton/MeasurementButton.tsx
@@ -7,6 +7,8 @@ import { useMeasurementStore } from "@/store/measurement/measurementStore";
 import { useDecibelMeasurement } from "@/hook/useDecibelMeasurement";
 import { useCallback, useRef } from "react";
 import { useRouter } from "next/navigation"; // useRouter 훅 추가
+import { useToastStore } from "@/store/toast/useToastStore";
+import { useAuthStore } from "@/store/auth/authStore";
 
 const buttonTransition = {
   initial: { opacity: 0, y: 50 },
@@ -23,8 +25,10 @@ export default function MeasurementButton() {
             addDecibel, 
             setCurrentDecibel 
     } = useMeasurementStore();
+    const addToast = useToastStore((state) => state.addToast);
 
     const { startMeasurement: startDecibel, stopMeasurement: stopDecibel } = useDecibelMeasurement();
+    const { accessToken } = useAuthStore();
 
     const router = useRouter(); // router 훅 추가
 
@@ -42,12 +46,21 @@ export default function MeasurementButton() {
     const handleCancel = useCallback(() => {
         stopDecibel(); // 마이크 종료
         cancelMeasurement(); // Zustand 상태 초기화
-    }, [stopDecibel, cancelMeasurement]);
+        addToast("측정이 취소되었습니다.", 2000);
+    }, [stopDecibel, cancelMeasurement, addToast]);
 
-    const handleSave = useCallback(() => {
+    const handleSave = useCallback(async () => {
         stopDecibel();
-        router.push('/register'); // 측정 저장 후 /register 이동
-    }, [stopDecibel, router]);
+
+        if (!accessToken) {
+            addToast("측정 데이터를 저장하려면 로그인이 필요합니다.", 2000);
+            cancelMeasurement(); // Zustand 상태 초기화
+            return;
+        }
+
+        addToast("저장 완료! 다음 단계로 이동합니다.", 2000);
+        router.push("/register");
+    }, [stopDecibel, cancelMeasurement, accessToken, addToast, router]);
 
     return (
         <AnimatePresence mode="wait" initial={false}>
