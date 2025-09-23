c2695e94f76ee9b7a8e6615024cbd765bee52e81
Elvin-lee <herjun802@gmail.com>
2025-09-24 00:47:52 +0900

    [Modified]: 중심 위치 MapSection prop로 전달하는 로직 추가

--- src/app/(main)/map/page.tsx
+++ src/app/(main)/map/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import FilterBottomSheet from "@/components/filterBottomSheet/FilterBottomSheet";
 import { MapHeader } from "@/components/header";
-
+import MapSection from "@/components/map/mapSection/MapSection";
+import { useMapLocationStore } from "@/store/map/useMapLocationStore";
 
 export default function MapPage() {
+    const { lat, lng } = useMapLocationStore();
+
     return (
         <>
             <MapHeader />
+            <main>
+                <MapSection centerLat={lat ?? undefined} centerLng={lng ?? undefined} />
+            </main>
             <FilterBottomSheet />
         </>
     );
