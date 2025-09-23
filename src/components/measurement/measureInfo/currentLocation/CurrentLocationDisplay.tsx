63785d34603fd6e6cc42b577821297afad6a32f2
Elvin-lee <herjun802@gmail.com>
2025-09-24 00:39:11 +0900

    [Modified]: searchList와 map 페이지 연동 searchList로부터 user가 클릭한 장소에 대한 lat, lng을 useMapLocationStore를 매개로 map페이지로 전달

--- src/components/measurement/measureInfo/currentLocation/CurrentLocationDisplay.tsx
+++ src/components/measurement/measureInfo/currentLocation/CurrentLocationDisplay.tsx
@@ -4,44 +4,39 @@ import { flexRow } from "@/mixin/style";
 import Image from "next/image";
 import { useEffect } from "react";
 import { useLocationStore } from "@/store/measurement/locationStore";
+import { useCurrentLocation } from "@/hook/useCurrentLocation";
 
 export default function CurrentLocationDisplay() {
   // store에서 location 상태와 setLocation 함수 가져오기
   const location = useLocationStore((state) => state.location);
   const setLocation = useLocationStore((state) => state.setLocation);
 
+  // custom hook 사용
+  const { lat, lng, error } = useCurrentLocation();
+
   useEffect(() => {
-    // 이미 장소 이름이 있으면 API 호출 생략
+    // 이미 값이 있다면 다시 호출 안 함
     if (location.placeName && location.placeName !== "위치 검색 중...") return;
 
-    if (!navigator.geolocation) {
+    if (error) {
       setLocation({
-        placeName: "위치 API 미지원",
-        location: { type: "Point", coordinates: null },
         kakaoPlaceId: null,
+        placeName: error,
+        location: { type: "Point", coordinates: null },
         categoryCode: null,
         categoryName: null,
       });
       return;
     }
 
-    const options = {
-      enableHighAccuracy: false,
-      timeout: 3000,
-      maximumAge: 60000,
-    };
-
-    navigator.geolocation.getCurrentPosition(
-      async (position) => {
-        const { latitude, longitude } = position.coords;
-
+    if (lat && lng) {
+      (async () => {
         try {
-          const response = await fetch(`/api/location?x=${longitude}&y=${latitude}`);
+          const response = await fetch(`/api/location?x=${lng}&y=${lat}`);
           if (!response.ok) throw new Error("서버에서 위치 정보 가져오기 실패");
 
           const data = await response.json();
 
-          // store에 PlaceSchema 구조로 저장
           setLocation({
             kakaoPlaceId: data.kakaoPlaceId,
             placeName: data.placeName,
@@ -59,19 +54,9 @@ export default function CurrentLocationDisplay() {
             categoryName: null,
           });
         }
-      },
-      () => {
-        setLocation({
-          kakaoPlaceId: null,
-          placeName: "위치 가져오기 실패",
-          location: { type: "Point", coordinates: null },
-          categoryCode: null,
-          categoryName: null,
-        });
-      },
-      options
-    );
-  }, [location.placeName, setLocation]);
+      })();
+    }
+  }, [lat, lng, error, location.placeName, setLocation]);
 
   return (
     <div
