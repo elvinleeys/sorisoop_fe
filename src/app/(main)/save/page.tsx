4ddec6652e59f8b49891f13311ee44cfbcf89068
Elvin-lee <herjun802@gmail.com>
2025-09-24 00:35:32 +0900

    [Modified]: sideBar 제거

--- src/app/(main)/save/page.tsx
+++ src/app/(main)/save/page.tsx
@@ -1,11 +1,5 @@
 import SaveMainPage from "@/components/saveMainPage/SaveMainPage";
-import SideBar from "@/components/sideBar/SideBar";
 
 export default function SavePage() {
-    return (
-        <>
-            <SaveMainPage />
-            <SideBar />
-        </>
-    );
+    return <SaveMainPage />;
 }
