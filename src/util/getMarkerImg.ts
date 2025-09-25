62aca90da6991868c69e52cb024e5e0afb45ddac
Elvin-lee <herjun802@gmail.com>
2025-09-26 00:01:36 +0900

    [Modified]: getMarkerImg 함수의 변수 null 추가 filter에 의해 현재 data가 없지만 kakaoMap의 category에 있는 좌표라면 default.svg로 표현하도록 변수에 null을 받을 수 있도록 추가

--- src/util/getMarkerImg.ts
+++ src/util/getMarkerImg.ts
@@ -1,5 +1,6 @@
-export function getMarkerImg(avgDecibel: number): string {
+export function getMarkerImg(avgDecibel: number | null): string {
+  if (avgDecibel === null) return "/icons/default.svg"; // 소음 데이터 없는 경우 기본 마커
   if (avgDecibel >= 100) return "/icons/loud.svg";
-  if (avgDecibel > 70) return "/icons/moderate.svg";
+  if (avgDecibel >= 70) return "/icons/moderate.svg";
   return "/icons/quiet.svg";
 }
