0619af30f4860926ee7a208bf1f0ca4e54ddf666
Elvin-lee <herjun802@gmail.com>
2025-09-28 23:02:28 +0900

    [Modified]: placeDetailSheet 관련 portal div 추가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,6 +12,10 @@ export default function MainLayout({ children } : MainLayoutProps) {
             <PageTransition>
                 {children}
             </PageTransition>
+            <div 
+                id="place-detail" 
+                className="fixed inset-x-0 bottom-[6.25rem] z-[9991]"
+            ></div>
             <NavBar />        
         </>
     );
