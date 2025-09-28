3968c58cb7973c911257e38cb2d823f81c186b3c
Elvin-lee <herjun802@gmail.com>
2025-09-28 23:02:58 +0900

    [Styles]: NavBar의 z index 변경

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
     const path = usePathname();
 
     return(
-        <div className="fixed bottom-0 left-0 w-full">
+        <div className="fixed bottom-0 left-0 w-full bg-white z-[9995]">
             <NavList items={navItems} currentPath={path}/>
         </div>
     );
