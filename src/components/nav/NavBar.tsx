41488de5851a618d32256794c8d622d535fd7be0
Elvin-lee <herjun802@gmail.com>
2025-10-01 00:35:32 +0900

    [Refactor]: NavBar의 div 태그 제거 soridam-design-system의 NavList에 z-index를 부여하고 div태그를 제거함으로써 DOM Depth를 줄임

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -7,9 +7,5 @@ import { usePathname } from "next/navigation";
 export default function NavBar() {
     const path = usePathname();
 
-    return(
-        <div className="fixed bottom-0 left-0 w-full bg-white z-[9995]">
-            <NavList items={navItems} currentPath={path}/>
-        </div>
-    );
+    return <NavList items={navItems} currentPath={path}/>;
 }
