dc890a08f88b5f6e824fb9e8a5d6491d1d8ddaf4
Elvin-lee <herjun802@gmail.com>
2025-10-01 00:32:21 +0900

    [Refactor]: section 태그 추가 검색엔진 최적화를 위해 중심 내용임을 알리기 위해 section 태그 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,13 +3,14 @@
 import { MainHeader } from "@/components/header";
 import MeasurementCard from "@/components/measurement/measurementCard/MeasurementCard";
 
-
 export default function Home() {
     return (
         <>
             <MainHeader />
             <main className="pr-[1.125rem] pl-[1.0625rem]">
-                <MeasurementCard />
+                <section>
+                    <MeasurementCard />
+                </section>
             </main>
         </>
     );
