3760a5b1e307a9b75da55ab5362a7cfb9b32954f
Elvin-lee <herjun802@gmail.com>
2025-10-02 00:11:38 +0900

    [Refactor]: RegisterForm 컴포넌트의 timSlot, resetRegisterState 함수 구현으로 코드를 간결화

--- src/components/register/registerForm/RegisterForm.tsx
+++ src/components/register/registerForm/RegisterForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { flexCol } from "@/mixin/style";
 import { useLocationStore } from "@/store/measurement/locationStore";
 import { useMeasurementStore } from "@/store/measurement/measurementStore";
 import { formatDateTime } from "@/util/formatDateTime";
@@ -9,49 +8,51 @@ import DecibelResult from "./decibelResult/DecibelResult";
 import ReviewInput from "./reviewInput/ReviewInput";
 import { useReviewStore } from "@/store/register/reviewStore";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useToastStore } from "@/store/toast/useToastStore";
 import { fetchWrapper } from "@/lib/fetchWrapper";
-
-
-const getDecibelLevel = (db: number) => {
-  if (db <= 70) return "quiet";
-  if (db < 100) return "moderate"; // 이미 db > 70 인 상황
-  return "loud"; // 나머지는 전부 100 이상
-};
+import { getDecibelLevel } from "@/util/getDecibelLevel";
 
 export default function RegisterForm() {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    
     const router = useRouter();
     const addToast = useToastStore((state) => state.addToast);
-    const { avgDecibel, maxDecibel, startedAt } = useMeasurementStore();
-    const { location } = useLocationStore();
+    const { avgDecibel, maxDecibel, startedAt, cancelMeasurement } = useMeasurementStore();
+    const { location, setLocation } = useLocationStore();
     const { placeName, kakaoPlaceId, location: geo, categoryCode, categoryName } = location;
-    const { value: comment, isValid, setSubmitAttempted } = useReviewStore();
-
-    const [isSubmitting, setIsSubmitting] = useState(false);
-
-    // 필수 데이터 없으면 redirect
-    useEffect(() => {
-        if (!avgDecibel || !maxDecibel || !startedAt || !geo?.coordinates) {
-            router.replace("/");
-        }
-    }, [avgDecibel, maxDecibel, startedAt, geo, router]);
+    const { value: comment, isValid, setSubmitAttempted, reset } = useReviewStore();
 
     if (!avgDecibel || !maxDecibel || !startedAt || !geo?.coordinates) return null;
 
     const decibelLevel = getDecibelLevel(avgDecibel);
     const { date, time } = formatDateTime(startedAt);
 
+    const getTimeSlot = (date: Date) => {
+        const hour = date.getHours();
+        if (hour >= 5 && hour < 11) return "5-11";
+        if (hour >= 11 && hour < 18) return "11-18";
+        if (hour >= 18 && hour < 22) return "18-22";
+        return "5-11";
+    };
+
+    const resetRegisterState = () => {
+        cancelMeasurement();
+        setLocation({
+            kakaoPlaceId: null,
+            placeName: "위치 검색 중...",
+            location: { type: "Point", coordinates: null },
+            categoryCode: null,
+            categoryName: null,
+        });
+        reset();
+    };
+
     const handleSubmit = async () => {
         setSubmitAttempted(true);
         if (!isValid) return;
 
         setIsSubmitting(true);
-
-        const hour = startedAt.getHours();
-        let timeSlot: "5-11" | "11-18" | "18-22" = "5-11";
-        if (hour >= 11 && hour < 18) timeSlot = "11-18";
-        else if (hour >= 18 && hour < 22) timeSlot = "18-22";
 
         const payload = {
             placeName,
@@ -61,7 +62,7 @@ export default function RegisterForm() {
             categoryName: categoryName || null,
             measuredAt: startedAt,
             measuredDate: date,
-            timeSlot,
+            timeSlot: getTimeSlot(startedAt),
             avgDecibel,
             maxDecibel,
             comment,
@@ -80,15 +81,7 @@ export default function RegisterForm() {
             );
 
             // ✅ Zustand 초기화
-            useMeasurementStore.getState().cancelMeasurement(); // 측정 데이터 초기화
-            useLocationStore.getState().setLocation({
-                kakaoPlaceId: null,
-                placeName: "위치 검색 중...",
-                location: { type: "Point", coordinates: null },
-                categoryCode: null,
-                categoryName: null,
-            });
-            useReviewStore.getState().reset();
+            resetRegisterState();
             router.push("/"); // 저장 후 이동
         } catch (err: unknown) {
             if (err instanceof Error) console.error(err.message);
@@ -100,7 +93,7 @@ export default function RegisterForm() {
 
     return (
         <section>
-            <form className={`${flexCol} gap-[1.25rem]`}>
+            <form className="flex flex-col gap-[1.25rem]">
                 <LocationInfo placeName={placeName!} decibelLevel={decibelLevel} date={date} time={time} />
                 <DecibelResult avgDecibel={avgDecibel} maxDecibel={maxDecibel} />
                 <ReviewInput onSubmit={handleSubmit} isSubmitting={isSubmitting} />
