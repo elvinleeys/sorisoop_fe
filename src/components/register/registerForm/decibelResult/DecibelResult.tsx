359c427cf2adb36ac6e0cf9916d0985f766e23d5
Elvin-lee <herjun802@gmail.com>
2025-10-02 00:32:05 +0900

    [Refactor]: 반복되는 style을 줄이기 위한 변수 선언

--- src/components/register/registerForm/decibelResult/DecibelResult.tsx
+++ src/components/register/registerForm/decibelResult/DecibelResult.tsx
@@ -7,20 +7,26 @@ interface DecibelResultProps {
     maxDecibel: number;
 }
 
-export default function DecibelResult({ avgDecibel, maxDecibel }: DecibelResultProps) {
+export default function DecibelResult({ 
+    avgDecibel, 
+    maxDecibel 
+}: DecibelResultProps) {
+    const boxStyle = "w-[6.25rem] h-[2.125rem] p-[0.3125rem] border border-neutral-gray-soft rounded-[0.375rem]";
+    const textStyle = "text-base text-black";
+
     return (
         <div className={`w-[13.5rem] ${flexCol} gap-[0.75rem]`}>
             <h3 className="w-full h-[1.5rem] text-base !font-bold text-neutral-black">
                 소음 측정 결과
             </h3>
             <div className={`w-full h-[2.125rem] ${flexRow} gap-[1rem]`}>
-                <div className="w-[6.25rem] h-[2.125rem] p-[0.3125rem] border border-neutral-gray-soft rounded-[0.375rem]">
-                    <p className="text-base text-black">
+                <div className={boxStyle}>
+                    <p className={textStyle}>
                         평균 {avgDecibel.toFixed(0)}
                     </p>
                 </div>
-                <div className="w-[6.25rem] h-[2.125rem] p-[0.3125rem] border border-neutral-gray-soft rounded-[0.375rem]">
-                    <p className="text-base text-black">
+                <div className={boxStyle}>
+                    <p className={textStyle}>
                         최대 {maxDecibel.toFixed(0)}
                     </p>
                 </div>
--- src/components/register/registerForm/locationInfo/LocationInfo.tsx
+++ src/components/register/registerForm/locationInfo/LocationInfo.tsx
@@ -11,6 +11,8 @@ interface LocationInfoProps {
 }
 
 export default function LocationInfo({ placeName, decibelLevel, date, time }: LocationInfoProps) {
+    const textStyle = "text-base text-neutral-sub"
+
     return (
         <article className={`${flexCol} gap-[0.25rem] w-full`}>
             <div className={`w-full h-[1.5rem] ${flexRowBetween}`}>
@@ -20,8 +22,8 @@ export default function LocationInfo({ placeName, decibelLevel, date, time }: Lo
                 <DecibelLabel level={decibelLevel} />
             </div>
             <div className={`w-full h-[1.5rem] flex gap-[0.25rem]`}>
-                <time className="text-base text-neutral-sub">{date}</time>
-                <time className="text-base text-neutral-sub">{time}</time>
+                <time className={textStyle}>{date}</time>
+                <time className={textStyle}>{time}</time>
             </div>
         </article>
     );
