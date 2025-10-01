b1a1f38f6fc72e37d019a15cbb709b0641b06377
Elvin-lee <herjun802@gmail.com>
2025-10-02 00:08:54 +0900

    [Refactor]: util함수(decibel로부터 level을 가져오는 함수 및 decibel로부터 img경로를 가져오는 함수) overloading으로 변경 측정 시 사용되는 함수와 여러 경로에서 avgDecibel로부터 level이나 img경로를 가져오는 함수가 반복됨에따라 overloading으로 변경

--- src/util/getDecibelLevel.ts
+++ src/util/getDecibelLevel.ts
@@ -1,15 +1,25 @@
+type MDecibelLevel = 'default' | 'quiet' | 'moderate' | 'loud';
+type DecibelLevel = 'quiet' | 'moderate' | 'loud';
+
+// 1. 오버로드 시그니처 (2가지 형태 정의)
+export function getDecibelLevel(db: number, status: string): MDecibelLevel;
+export function getDecibelLevel(db: number): DecibelLevel;
+
 // 데시벨 값에 따라 레벨을 결정하는 함수
-export function getDecibelLevel(db: number, status: string) {
+export function getDecibelLevel(db: number, status?: string) {
   if (status === "idle") return "default";
   if (db <= 70) return "quiet";
   if (db > 70 && db < 100) return "moderate";
-  if (db >= 100) return "loud";
-  return "default";
+  return "loud";
 };
+
+// 1. 오버로드 시그니처 (2가지 형태 정의)
+export function getMarkerImg(avgDecibel: number | null): string;
+export function getMarkerImg(db: number): string;
 
 export function getMarkerImg(avgDecibel: number | null): string {
   if (avgDecibel === null) return "/icons/default.svg"; // 소음 데이터 없는 경우 기본 마커
-  if (avgDecibel >= 100) return "/icons/loud.svg";
-  if (avgDecibel >= 70) return "/icons/moderate.svg";
-  return "/icons/quiet.svg";
+  if (avgDecibel >= 100) return '/icons/loud.svg';
+  if (avgDecibel >= 70) return '/icons/moderate.svg'; // 70 <= db < 100
+  return '/icons/quiet.svg'; // db < 70
 }
