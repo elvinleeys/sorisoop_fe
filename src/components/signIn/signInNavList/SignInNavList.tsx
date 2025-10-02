fdcad4451c6538859c484d7aee5b60b72964f931
Elvin-lee <herjun802@gmail.com>
2025-10-03 00:50:46 +0900

    [Modified]: 미지원 서비스에 대한 toast 알림 추가

--- src/components/signIn/signInNavList/SignInNavList.tsx
+++ src/components/signIn/signInNavList/SignInNavList.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import { flexRowCenter } from "@/mixin/style";
+import { useToastStore } from "@/store/toast/useToastStore";
 import Link from "next/link";
 
 export default function SignInNavList() {
+    const addToast = useToastStore((s) => s.addToast);
+
+    const handleClick = (label: string) => {
+        addToast(`${label} 기능은 현재 제공되지 않습니다.`);
+    }
+
     return (
         <nav className={`${flexRowCenter} text-sm text-neutral-gray gap-3`}>
-            <Link href="#">아이디 찾기</Link>
+            <button 
+                onClick={() => handleClick("아이디 찾기")}
+            >
+                아이디 찾기
+            </button>
             <span>|</span>
-            <Link href="#">비밀번호 찾기</Link>
+            <button 
+                onClick={() => handleClick("비밀번호 찾기")}
+            >
+                비밀번호 찾기
+            </button>
             <span>|</span>
             <Link href="/sign-up">회원가입</Link>
         </nav>
