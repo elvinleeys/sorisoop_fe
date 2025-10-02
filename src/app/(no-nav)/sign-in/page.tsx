c6e0b1683676eb01d1aa2e8ec8d1117459cdbb23
Elvin-lee <herjun802@gmail.com>
2025-10-03 00:50:23 +0900

    [styles]: main 태그의 중앙 정렬 추가

--- src/app/(no-nav)/sign-in/page.tsx
+++ src/app/(no-nav)/sign-in/page.tsx
@@ -2,12 +2,19 @@ import { SignInHeader } from "@/components/header";
 import SignInLogo from "@/components/signIn/logo/SignInLogo";
 import SignInForm from "@/components/signIn/signInForm/SignInForm";
 import SignInNavList from "@/components/signIn/signInNavList/SignInNavList";
+import { flexColCenter } from "@/mixin/style";
 
 export default function SignInPage() {
     return (
         <>
             <SignInHeader />
-            <main className="px-4">
+            <main 
+                className={`
+                    ${flexColCenter} 
+                    min-h-screen 
+                    px-4
+                `}
+            >
                 <SignInLogo />
                 <SignInForm />
                 <SignInNavList />
