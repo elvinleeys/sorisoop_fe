ec92c824875d10d7e0e9d2af4fbf4cee7bff8c65
Elvin-lee <herjun802@gmail.com>
2025-10-03 00:42:48 +0900

    [Refactor]: 반복되는 css style 변수로 정리

--- src/components/save/noData/NoData.tsx
+++ src/components/save/noData/NoData.tsx
@@ -2,14 +2,16 @@ import { flexColCenter } from "@/mixin/style";
 import NoDataImg from "../noDataImg/NoDataImg";
 
 export default function NoData() {
+    const textStyle = "text-base";
+
     return (
         <section className={`${flexColCenter} gap-8 mt-25.5`}>
             <NoDataImg />
             <div className="text-center text-neutral-black">
-                <p className="text-base">
+                <p className={textStyle}>
                     저장된 데이터가 없습니다.
                 </p>
-                <p className="text-base">
+                <p className={textStyle}>
                     소음을 측정하고 데이터를 저장해보세요!
                 </p>
             </div>
