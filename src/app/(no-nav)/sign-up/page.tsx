d0fdb6a023e30bda606ad6c2b357f17b29038c7d
Elvin-lee <herjun802@gmail.com>
2025-10-04 14:58:57 +0900

    [Modified]: 브라우저의 뒤로가기 버튼 클릭시 데이터 clear 동작 추가

--- src/app/(no-nav)/sign-up/page.tsx
+++ src/app/(no-nav)/sign-up/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import StepOneForm from "@/components/sign-up/step1/StepOneForm";
+import { useSignUpStore } from "@/store/signUp/SignUpStore";
+import { useEffect } from "react";
 
 export default function SignUpPage() {
-    return (
-        <>
-            <StepOneForm />
-        </>
-    );
+    const resetSignUp = useSignUpStore((state) => state.reset);
+
+    useEffect(() => {
+        resetSignUp(); // ✅ 첫 진입 시 상태 초기화
+    }, [resetSignUp]);
+
+    return <StepOneForm />;
 }
