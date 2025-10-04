488be64870afd5beba4f16c2638a940bf8bce16e
Elvin-lee <herjun802@gmail.com>
2025-10-04 14:58:03 +0900

    [Styles]: logo 위치 수정

--- src/app/(no-nav)/sign-in/page.tsx
+++ src/app/(no-nav)/sign-in/page.tsx
@@ -2,19 +2,12 @@ import { SignInHeader } from "@/components/header";
 import SignInLogo from "@/components/signIn/logo/SignInLogo";
 import SignInForm from "@/components/signIn/signInForm/SignInForm";
 import SignInNavList from "@/components/signIn/signInNavList/SignInNavList";
-import { flexColCenter } from "@/mixin/style";
 
 export default function SignInPage() {
     return (
         <>
             <SignInHeader />
-            <main 
-                className={`
-                    ${flexColCenter} 
-                    min-h-screen 
-                    px-4
-                `}
-            >
+            <main className="min-h-screen px-4">
                 <SignInLogo />
                 <SignInForm />
                 <SignInNavList />
