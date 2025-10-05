3cf75839b93deb487090b543d4bc3bdc14b6bfbe
Elvin-lee <herjun802@gmail.com>
2025-10-05 15:28:17 +0900

    [Refactor]: User schema에 대한 type 추가

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
+// User 스키마에 타입 지정
+export interface IUserDocument extends mongoose.Document {
+  _id: mongoose.Types.ObjectId;
+  nickname: string;
+  email: string;
+  password: string;
+  profileImg?: string;
+  refreshToken?: {
+    token: string;
+    expiredAt?: Date | null;
+  };
+}
+
 const UserSchema = new mongoose.Schema({
     nickname: { type: String, required: true },
     email: { type: String, required: true },
