7629a345a5818c2eb331554a2cbc5ea841d12a3e
elvinleeys <herjun802@gmail.com>
2026-05-24 14:32:59 +0900

    [type]: kakaoMap 관련 type 추가

---
+++ src/shared/types/kakaoMap.ts
@@ -0,0 +1,30 @@
+export interface MapMarker {
+    id?: string;
+    lat: number;
+    lng: number;
+    image?: string;
+}
+
+export interface MarkerInstance {
+    marker: kakao.maps.Marker;
+    image?: string;
+}
+
+export interface MapControllerOptions {
+    lat: number;
+    lng: number;
+    level?: number;
+    draggable?: boolean;
+    zoomable?: boolean;
+}
+
+export interface MapController {
+    map: kakao.maps.Map;
+    setCenter(lat: number, lng: number): void;
+    setLevel(level: number): void;
+}
+
+export type MarkerImageCache = (
+    src: string,
+    size: number,
+) => kakao.maps.MarkerImage;
