64f68c90f6bfa6b0ad961215f484c85a08361395
elvinleeys <herjun802@gmail.com>
2026-05-26 15:21:33 +0900

    [Refactor]: 현재 위치 가져오는 로직 교체

--- src/app/(no-nav)/map/search/page.tsx
+++ src/app/(no-nav)/map/search/page.tsx
@@ -2,7 +2,7 @@
 
 import { SearchHeader } from "@/components/header";
 import SearchList from "@/components/searchList/SearchList";
-import { useCurrentLocation } from "@/hook/useCurrentLocation";
+import { useExploreLocationStore } from "@/features/map/explore/model/store/exploreLocationStore";
 import { fetchKakaoPlaces } from "@/services/search/fetchKakaoPlace";
 import { KakaoPlace } from "@/types/kakaoPlace";
 import { useQuery } from "@tanstack/react-query";
@@ -10,19 +10,30 @@ import { useState } from "react";
 
 export default function Search() {
     const [keyword, setKeyword] = useState("");
-    const { lat, lng } = useCurrentLocation();
+    const { lat, lng } = useExploreLocationStore();
 
-    const { data: places = [], refetch, isFetching } = useQuery<KakaoPlace[]>({
+    const {
+        data: places = [],
+        refetch,
+        isFetching,
+    } = useQuery<KakaoPlace[]>({
         queryKey: ["places", keyword, lat, lng],
         queryFn: () => fetchKakaoPlaces(keyword, lat!, lng!),
         enabled: false, // Enter 하기 전엔 실행 안 함
     });
 
-
     return (
         <>
-            <SearchHeader keyword={keyword} setKeyword={setKeyword} onSearch={refetch} />
-            <SearchList keyword={keyword} places={places} isFetching={isFetching} />
+            <SearchHeader
+                keyword={keyword}
+                setKeyword={setKeyword}
+                onSearch={refetch}
+            />
+            <SearchList
+                keyword={keyword}
+                places={places}
+                isFetching={isFetching}
+            />
         </>
     );
 }
